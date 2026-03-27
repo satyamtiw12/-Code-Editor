b7b3d9d8d92cfc74921325310ce6a97463baf282
Satyam <satyamtiwari9128@gmail.com>
2026-03-27 21:52:46 +0530

    fix route star error

--- backend/index.js
+++ backend/index.js
@@ -219,11 +219,17 @@ app.post("/run", (req, res) => {
   }
 });
 
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+
 const frontendPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
